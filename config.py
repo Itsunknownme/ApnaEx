00dbd8937c5b78033f2a7cfa0b3461499d15e437
Itsunknownme <155176751+Itsunknownme@users.noreply.github.com>
2025-11-19 15:55:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,51 +1,37 @@
 import os
 from os import getenv
 
+# ------------------------------------------------
+# Basic Bot Configuration (Environment Variables)
+# ------------------------------------------------
 
-# ------------------------------------------------
 API_ID = int(os.environ.get("API_ID", ""))
-# ------------------------------------------------
-API_HASH = os.environ.get("API_HASH","")
-# ------------------------------------------------
+API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-# ------------------------------------------------
 BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
 BOT_TEXT = "EXTRACTOR"
-# ------------------------------------------------
+
 OWNER_ID = int(os.environ.get("OWNER_ID", ""))
-# ------------------------------------------------
-# //LOG CHANNEL ID 
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
 
-# //FORCE_CHANNEL_ID
-#CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "")) 
-# ------------------------------------------------
+# Optional Force Join Channel
+# CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", ""))
+
+# Database
 MONGO_URL = os.environ.get("MONGO_URL", "")
-# -----------------------------------------------
+
+# Logging Channels
 PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", ""))
-# -----------------------------------------------
-#join = '<a href="https://t.me/RPSC_UPSC_CURRENT">✳️ JOIN BACKUP</a>'
-# -----------------------------------------------
-UNSPLASH_ACCESS_KEY = 'RabDRmuXXBobanmwwbvpP5LwoG4J8ox34y5Sstz-9jk'
-# -----------------------------------------------
-UNSPLASH_QUERY = 'animal baby'
-# -----------------------------------------------
-#ADMIN_BOT_USERNAME = "Mirzahb7_Ghallib" #without @
 
-THUMB_URL = os.environ.get("THUMB_URL", "https://josephscollege.ac.in/wp-content/uploads/2022/04/1.jpg")
+# ------------------------------------------------
+# External API Keys / Other Settings
+# ------------------------------------------------
 
+UNSPLASH_ACCESS_KEY = "RabDRmuXXBobanmwwbvpP5LwoG4J8ox34y5Sstz-9jk"
+UNSPLASH_QUERY = "animal baby"
 
-
-
-# # Bot configuration
-# API_ID = int(os.environ.get("API_ID", "21157244"))
-# API_HASH = os.environ.get("API_HASH", "4981c2699bd91c7db836ec8f77e5b0f0")
-# BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-# BOT_USERNAME = os.environ.get("BOT_USERNAME", "Gajjubhaikaext_bot")
-# OWNER_ID = int(os.environ.get("OWNER_ID", "1783306092"))
-# SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1783306092").split()))
-# CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002195125473"))
-# MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://wadiro6523:08AwfhhKRdQaS1i6@cluster0.krzxuop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-# PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002195125473"))
-# THUMB_URL = os.environ.get("THUMB_URL", "https://i.fbcd.co/products/original/ug-circle-logo-design-2-e84695ca2ab9a697d2b2d7c928b0bf5f12bf18e076da241815e0372c8d617915.jpg")
-
+# Thumbnail
+THUMB_URL = os.environ.get(
+    "THUMB_URL",
+    "https://josephscollege.ac.in/wp-content/uploads/2022/04/1.jpg"
+)
